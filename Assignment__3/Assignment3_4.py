402c8c0385042118df842a00b05b7743d337a665
AshaShankarWadgmode <ashawaghmode06@gmail.com>
2026-05-12 22:23:14 +0530

    Add files via upload

---
+++ Assignment__3/Assignment3_4.py
@@ -0,0 +1,16 @@
+
+
+def DisplayConvert (Cvalue):
+    if 'A' <= Cvalue <= 'Z':
+        print(Cvalue.lower())
+    else:
+        print(Cvalue.upper())
+    print()
+def main():
+    
+    print("Enter Character  : ")
+    cValue = input().strip()
+    DisplayConvert(cValue)
+
+if __name__ =="__main__":
+    main()
